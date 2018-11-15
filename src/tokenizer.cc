b7976d3fc7106e887f0a792270dd0148df59209e
dan sinclair <dsinclair@chromium.org>
2018-11-15 17:10:08 -0500

    Windows build

--- src/tokenizer.cc
+++ src/tokenizer.cc
@@ -14,6 +14,7 @@
 
 #include "src/tokenizer.h"
 
+#include <cctype>
 #include <cstdlib>
 #include <limits>
 #include <sstream>
@@ -34,10 +35,11 @@ Result Token::ConvertToDouble() {
     return Result("Invalid conversion to double");
 
   if (IsInteger()) {
-    if (is_negative_ || uint_value_ <= std::numeric_limits<int64_t>::max())
+    if (is_negative_ || uint_value_ <= static_cast<uint64_t>(std::numeric_limits<int64_t>::max())) {
       double_value_ = static_cast<double>(AsInt64());
-    else
+    } else {
       return Result("uint64_t value too big to fit in double");
+    }
 
     uint_value_ = 0;
   } else if (IsHex()) {
