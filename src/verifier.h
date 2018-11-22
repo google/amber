d485c4648cbc3500f959184b20fe6182c27a4ad3
Dan Sinclair <dsinclair@chromium.org>
2018-11-22 17:00:08 -0500

    fix lint

--- src/verifier.h
+++ src/verifier.h
@@ -12,6 +12,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#ifndef SRC_VERIFIER_H_
+#define SRC_VERIFIER_H_
+
 #include "amber/result.h"
 #include "src/command.h"
 
@@ -32,3 +35,5 @@ class Verifier {
 };
 
 }  // namespace amber
+
+#endif  // SRC_VERIFIER_H_
