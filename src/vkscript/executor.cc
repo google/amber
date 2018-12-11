c666e8c08dd78e211152d0566700a5cdb1312eec
dan sinclair <dj2@everburning.com>
2018-12-11 11:12:12 -0500

    fix formatting

--- src/vkscript/executor.cc
+++ src/vkscript/executor.cc
@@ -66,8 +66,7 @@ Result Executor::Execute(Engine* engine,
     }
 
     Result r = engine->SetBuffer(
-        buf->GetBufferType(),
-        buf->GetLocation(),
+        buf->GetBufferType(), buf->GetLocation(),
         buf->IsFormatBuffer() ? buf->AsFormatBuffer()->GetFormat() : Format(),
         buf->GetData());
     if (!r.IsSuccess())
