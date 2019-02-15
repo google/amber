c9e38c96c278c2fd3037ce7b7dd01927fbe073b8
Jaebaek Seo <jaebaek@google.com>
2019-02-15 18:34:42 +0900

    WIP

--- src/amber.cc
+++ src/amber.cc
@@ -20,6 +20,7 @@
 #include <string>
 
 #include "src/amberscript/parser.h"
+#include "src/descriptor_set_and_binding_parser.h"
 #include "src/engine.h"
 #include "src/executor.h"
 #include "src/make_unique.h"
@@ -27,64 +28,6 @@
 #include "src/vkscript/parser.h"
 
 namespace amber {
-namespace {
-
-// Parse |buffer_id| and return desciptor set and binding using |desc_set|
-// and |binding|. |buffer_id| must be non-empty string. It must be a single
-// non-negative integer or two those integers separated by ':'. For example,
-// ":0", "1", and "2:3" are valid strings for |buffer_id|, but "", "-4",
-// ":-5", ":", "a", and "b:c" are invalid strings for |buffer_id|.
-amber::Result ParseDescriptorSetAndBinding(const std::string& buffer_id,
-                                           uint32_t* desc_set,
-                                           uint32_t* binding) {
-  if (buffer_id.empty())
-    return Result("Given buffer to dump has an empty name.");
-
-  size_t pos = 0;
-  while (pos < buffer_id.size() && std::isdigit(buffer_id[pos]))
-    ++pos;
-
-  // |buffer_id| is a single integer. It must be non-negative.
-  if (pos >= buffer_id.size()) {
-    *desc_set = 0;
-    int32_t binding_before_validation =
-        static_cast<int32_t>(std::strtoull(buffer_id.c_str(), nullptr, 10));
-    if (binding_before_validation < 0) {
-      return Result("binding for buffer must be positive, got: " +
-                    std::to_string(binding_before_validation));
-    }
-    *binding = static_cast<uint32_t>(binding_before_validation);
-    return {};
-  }
-
-  // |buffer_id| contains a character that is not ':'.
-  if (buffer_id[pos] != ':')
-    return Result("Invalid name of buffer '" + buffer_id + "' to dump.");
-
-  // |buffer_id| is ":".
-  if (pos + 1 >= buffer_id.size())
-    return Result("Invalid name of buffer '" + buffer_id + "' to dump.");
-
-  // |buffer_id| has two integers. They must be non-negative.
-  int32_t number_before_validation = static_cast<int32_t>(
-      std::strtoull(buffer_id.substr(0, pos).c_str(), nullptr, 10));
-  if (number_before_validation < 0) {
-    return Result("binding for buffer must be positive, got: " +
-                  std::to_string(number_before_validation));
-  }
-  *desc_set = static_cast<uint32_t>(number_before_validation);
-
-  number_before_validation = static_cast<int32_t>(
-      std::strtoull(buffer_id.substr(pos + 1).c_str(), nullptr, 10));
-  if (number_before_validation < 0) {
-    return Result("binding for buffer must be positive, got: " +
-                  std::to_string(number_before_validation));
-  }
-  *binding = static_cast<uint32_t>(number_before_validation);
-  return {};
-}
-
-}  // namespace
 
 Amber::Amber() = default;
 
@@ -161,17 +104,17 @@ amber::Result Amber::ExecuteWithShaderData(const amber::Recipe* recipe,
       continue;
     }
 
-    uint32_t desc_set = 0;
-    uint32_t binding = 0;
-    r = ParseDescriptorSetAndBinding(buffer_info.buffer_name, &desc_set,
-                                     &binding);
+    DescriptorSetAndBindingParser desc_set_and_binding_parser;
+    r = desc_set_and_binding_parser.Parse(buffer_info.buffer_name);
     if (!r.IsSuccess()) {
       engine->Shutdown();
       return r;
     }
 
     ResourceInfo info = ResourceInfo();
-    r = engine->GetDescriptorInfo(desc_set, binding, &info);
+    r = engine->GetDescriptorInfo(
+        desc_set_and_binding_parser.GetDescriptorSet(),
+        desc_set_and_binding_parser.GetBinding(), &info);
     if (!r.IsSuccess()) {
       engine->Shutdown();
       return r;
