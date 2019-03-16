25afda3bd8ec8cb972c971b5854498b18ed823e0
dan sinclair <dj2@everburning.com>
2019-03-16 00:12:18 -0400

    [vulkan] More MoltenVK suppressions.
    This CL suppresses two more tests due to lack of std140 support for
    matrices in SPIRV-Cross.
    
        "compute_mat2x2.vkscript",
        "compute_mat3x2.vkscript"
    
    Issue #216.

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -28,7 +28,11 @@ SUPPRESSIONS = {
     # No geometry shader on MoltenVK
     "draw_triangle_list_using_geom_shader.vkscript",
     # No tessellation shader on MoltenVK
-    "draw_triangle_list_using_tessellation.vkscript"
+    "draw_triangle_list_using_tessellation.vkscript",
+    # No std140 support for matrices in SPIRV-Cross
+    "compute_mat2x2.vkscript",
+    # No std140 support for matrices in SPIRV-Cross
+    "compute_mat3x2.vkscript"
   ]
 }
 
