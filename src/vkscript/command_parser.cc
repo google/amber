cb2c11a0dd3a78a8ebcd4d22ae53d6239032ce5c
SarahM0 <sarahmashay@google.com>
2019-06-25 14:07:39 -0400

    don't set the data

--- src/vkscript/command_parser.cc
+++ src/vkscript/command_parser.cc
@@ -615,7 +615,21 @@ Result CommandParser::ProcessSSBO() {
     if (!r.IsSuccess())
       return r;
 
-    buf->SetDataWithOffset(values, cmd->GetOffset());
+    // Multiply by the input needed because the value count will use the needed
+    // input as the multiplier
+    uint32_t value_count =
+        ((cmd->GetOffset() / buf->GetFormat()->SizeInBytes()) *
+         buf->GetFormat()->InputNeededPerElement()) +
+        static_cast<uint32_t>(values.size());
+    // The buffer should only be resized to become bigger. This means that if a
+    // command was run to set the buffer size we'll honour that size until a
+    // request happens to make the buffer bigger.
+    if (value_count > buf->ValueCount())
+      buf->SetValueCount(value_count);
+
+    // Even if the value count doesn't change, the buffer is still resized
+    // because this maybe the first time data is set into the buffer.
+    buf->ResizeTo(buf->GetSizeInBytes());
 
     cmd->SetValues(std::move(values));
 
