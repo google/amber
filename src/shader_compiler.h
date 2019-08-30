01e0396711489002b00255073d1cdec467d71803
jarisiru <53901722+jarisiru@users.noreply.github.com>
2019-08-30 15:38:22 +0300

    Remove unused private variable

--- src/shader_compiler.h
+++ src/shader_compiler.h
@@ -30,7 +30,7 @@ namespace amber {
 class ShaderCompiler {
  public:
   ShaderCompiler();
-  ShaderCompiler(const std::string& env, bool disable_spirv_validation);
+  ShaderCompiler(const std::string& env);
   ~ShaderCompiler();
 
   /// Returns a result code and a compilation of the given shader.
@@ -53,7 +53,6 @@ class ShaderCompiler {
                         std::vector<uint32_t>* result) const;
 
   std::string spv_env_;
-  bool disable_spirv_validation_ = false;
 };
 
 // Parses the SPIR-V environment string, and returns the corresponding
