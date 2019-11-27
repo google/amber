5377284a5f53605a1e3e5351fbb00c1baa36d64d
Dan Sinclair <dsinclair@google.com>
2019-11-27 16:40:21 -0500

    full test program path on windows

--- kokoro/scripts/windows/build.bat
+++ kokoro/scripts/windows/build.bat
@@ -69,7 +69,7 @@ echo "%DATE% %TIME%: Starting integration tests.."
 set VK_LAYER_PATH=build\third_party\vulkan-validationlayers\layers
 set VK_ICD_FILENAMES=build\Windows\vk_swiftshader_icd.json
 cd %SRC%
-python tests\run_tests.py --build-dir=%SRC%\build --use-swiftshader
+python tests\run_tests.py --test-prog-path=%SRC%\build\amber.exe --use-swiftshader
 echo "%DATE% %TIME%: integration tests completed."
 
 exit /b %ERRORLEVEL%
