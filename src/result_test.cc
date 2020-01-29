58eb5dabb3fcb244862b4a66fdd02d9da6fc7079
Ben Clayton <bclayton@google.com>
2020-01-29 21:39:35 +0000

    Result: Support composite results (#775)
    This change adds the `+=` operator to `amber::Result`, allowing results to contain more than one error message.
    This simplifies the accumulation of errors produced by concurrent testing.
    
    Also make `Result::Error()` return `"<no error message given>"` if a `Result` is constructed with an empty string.
    
    Added tests.

--- src/result_test.cc
+++ src/result_test.cc
@@ -31,12 +31,85 @@ TEST_F(ResultTest, ErrorWithString) {
   EXPECT_EQ("Test Failed", r.Error());
 }
 
+TEST_F(ResultTest, ErrorWithEmptyString) {
+  Result r("");
+  EXPECT_FALSE(r.IsSuccess());
+  EXPECT_EQ("<no error message given>", r.Error());
+}
+
 TEST_F(ResultTest, Copy) {
   Result r("Testing");
   Result r2(r);
 
   EXPECT_FALSE(r2.IsSuccess());
   EXPECT_EQ("Testing", r2.Error());
+}
+
+TEST_F(ResultTest, Append1String) {
+  Result r;
+  r += "Test Failed";
+  EXPECT_EQ("Test Failed", r.Error());
+}
+
+TEST_F(ResultTest, Append3Strings) {
+  Result r;
+  r += "Error one";
+  r += "Error two";
+  r += "Error three";
+  EXPECT_EQ(R"(3 errors:
+ (1) Error one
+ (2) Error two
+ (3) Error three)",
+            r.Error());
+}
+
+TEST_F(ResultTest, Append1SingleErrorResult) {
+  Result r;
+  r += Result("Test Failed");
+  EXPECT_EQ("Test Failed", r.Error());
+}
+
+TEST_F(ResultTest, Append3SingleErrorResults) {
+  Result r;
+  r += Result("Error one");
+  r += Result("Error two");
+  r += Result("Error three");
+  EXPECT_EQ(R"(3 errors:
+ (1) Error one
+ (2) Error two
+ (3) Error three)",
+            r.Error());
+}
+
+TEST_F(ResultTest, Append3MixedResults) {
+  Result r;
+  r += Result("Error one");
+  r += Result();  // success
+  r += Result("Error two");
+  EXPECT_EQ(R"(2 errors:
+ (1) Error one
+ (2) Error two)",
+            r.Error());
+}
+
+TEST_F(ResultTest, AppendMultipleErrorResults) {
+  Result r1;
+  r1 += Result("r1 error one");
+  r1 += Result("r1 error two");
+  r1 += Result("r1 error three");
+  r1 += Result("");
+  Result r2;
+  r2 += Result("r2 error one");
+  r2 += r1;
+  r2 += Result("r2 error two");
+  EXPECT_EQ(R"(6 errors:
+ (1) r2 error one
+ (2) r1 error one
+ (3) r1 error two
+ (4) r1 error three
+ (5) <no error message given>
+ (6) r2 error two)",
+            r2.Error());
 }
 
 }  // namespace amber
