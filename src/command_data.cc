1a7228a4781e11d7e8353eefc801f1a268cb6763
Ari Suonpää <ari.suonpaa@siru.fi>
2021-11-17 08:41:02 +0200

    Another build error fix.

--- src/command_data.cc
+++ src/command_data.cc
@@ -13,7 +13,6 @@
 // limitations under the License.
 
 #include "src/command_data.h"
-#include <map>
 
 namespace amber {
 
@@ -56,93 +55,153 @@ Topology NameToTopology(const std::string& name) {
 }
 
 BlendFactor NameToBlendFactor(const std::string& name) {
-  static const std::map<const std::string, BlendFactor> map = {
-      {"zero", BlendFactor::kZero},
-      {"one", BlendFactor::kOne},
-      {"src_color", BlendFactor::kSrcColor},
-      {"one_minus_src_color", BlendFactor::kOneMinusSrcColor},
-      {"dst_color", BlendFactor::kDstColor},
-      {"one_minus_dst_color", BlendFactor::kOneMinusDstColor},
-      {"src_alpha", BlendFactor::kSrcAlpha},
-      {"one_minus_src_alpha", BlendFactor::kOneMinusSrcAlpha},
-      {"dst_alpha", BlendFactor::kDstAlpha},
-      {"one_minus_dst_alpha", BlendFactor::kOneMinusDstAlpha},
-      {"constant_color", BlendFactor::kConstantColor},
-      {"one_minus_constant_color", BlendFactor::kOneMinusConstantColor},
-      {"costant_alpha", BlendFactor::kConstantAlpha},
-      {"one_minus_constant_alpha", BlendFactor::kOneMinusConstantAlpha},
-      {"src_alpha_saturate", BlendFactor::kSrcAlphaSaturate},
-      {"src1_color", BlendFactor::kSrc1Color},
-      {"one_minus_src1_color", BlendFactor::kOneMinusSrc1Color},
-      {"src1_alpha", BlendFactor::kSrc1Alpha},
-      {"one_minus_src1_alpha", BlendFactor::kOneMinusSrc1Alpha}};
-
-  auto item = map.find(name);
-  if (item != map.end())
-    return item->second;
-
-  return BlendFactor::kUnknown;
+  if (name == "zero")
+    return BlendFactor::kZero;
+  else if (name == "one")
+    return BlendFactor::kOne;
+  else if (name == "src_color")
+    return BlendFactor::kSrcColor;
+  else if (name == "one_minus_src_color")
+    return BlendFactor::kOneMinusSrcColor;
+  else if (name == "dst_color")
+    return BlendFactor::kDstColor;
+  else if (name == "one_minus_dst_color")
+    return BlendFactor::kOneMinusDstColor;
+  else if (name == "src_alpha")
+    return BlendFactor::kSrcAlpha;
+  else if (name == "one_minus_src_alpha")
+    return BlendFactor::kOneMinusSrcAlpha;
+  else if (name == "dst_alpha")
+    return BlendFactor::kDstAlpha;
+  else if (name == "one_minus_dst_alpha")
+    return BlendFactor::kOneMinusDstAlpha;
+  else if (name == "constant_color")
+    return BlendFactor::kConstantColor;
+  else if (name == "one_minus_constant_color")
+    return BlendFactor::kOneMinusConstantColor;
+  else if (name == "costant_alpha")
+    return BlendFactor::kConstantAlpha;
+  else if (name == "one_minus_constant_alpha")
+    return BlendFactor::kOneMinusConstantAlpha;
+  else if (name == "src_alpha_saturate")
+    return BlendFactor::kSrcAlphaSaturate;
+  else if (name == "src1_color")
+    return BlendFactor::kSrc1Color;
+  else if (name == "one_minus_src1_color")
+    return BlendFactor::kOneMinusSrc1Color;
+  else if (name == "src1_alpha")
+    return BlendFactor::kSrc1Alpha;
+  else if (name == "one_minus_src1_alpha")
+    return BlendFactor::kOneMinusSrc1Alpha;
+  else
+    return BlendFactor::kUnknown;
 }
 
 BlendOp NameToBlendOp(const std::string& name) {
-  static const std::map<const std::string, BlendOp> map = {
-      {"add", BlendOp::kAdd},
-      {"substract", BlendOp::kSubtract},
-      {"reverse_substract", BlendOp::kReverseSubtract},
-      {"min", BlendOp::kMin},
-      {"max", BlendOp::kMax},
-      {"zero", BlendOp::kZero},
-      {"src", BlendOp::kSrc},
-      {"dst", BlendOp::kDst},
-      {"src_over", BlendOp::kSrcOver},
-      {"dst_over", BlendOp::kDstOver},
-      {"src_in", BlendOp::kSrcIn},
-      {"dst_in", BlendOp::kDstIn},
-      {"src_out", BlendOp::kSrcOut},
-      {"dst_out", BlendOp::kDstOut},
-      {"src_atop", BlendOp::kSrcAtop},
-      {"dst_atop", BlendOp::kDstAtop},
-      {"xor", BlendOp::kXor},
-      {"multiply", BlendOp::kMultiply},
-      {"screen", BlendOp::kScreen},
-      {"overlay", BlendOp::kOverlay},
-      {"darken", BlendOp::kDarken},
-      {"lighten", BlendOp::kLighten},
-      {"color_dodge", BlendOp::kColorDodge},
-      {"color_burn", BlendOp::kColorBurn},
-      {"hard_light", BlendOp::kHardLight},
-      {"soft_light", BlendOp::kSoftLight},
-      {"difference", BlendOp::kDifference},
-      {"exclusion", BlendOp::kExclusion},
-      {"invert", BlendOp::kInvert},
-      {"invert_rgb", BlendOp::kInvertRGB},
-      {"linear_dodge", BlendOp::kLinearDodge},
-      {"linear_burn", BlendOp::kLinearBurn},
-      {"vivid_light", BlendOp::kVividLight},
-      {"linear_light", BlendOp::kLinearLight},
-      {"pin_light", BlendOp::kPinLight},
-      {"hard_mix", BlendOp::kHardMix},
-      {"hsl_hue", BlendOp::kHslHue},
-      {"hsl_saturation", BlendOp::kHslSaturation},
-      {"hsl_color", BlendOp::kHslColor},
-      {"hsl_luminosity", BlendOp::kHslLuminosity},
-      {"plus", BlendOp::kPlus},
-      {"plus_clamped", BlendOp::kPlusClamped},
-      {"plus_clamped_alpha", BlendOp::kPlusClampedAlpha},
-      {"plus_darker", BlendOp::kPlusDarker},
-      {"minus", BlendOp::kMinus},
-      {"minus_clamped", BlendOp::kMinusClamped},
-      {"contrast", BlendOp::kContrast},
-      {"invert_ovg", BlendOp::kInvertOvg},
-      {"red", BlendOp::kRed},
-      {"green", BlendOp::kGreen},
-      {"blue", BlendOp::kBlue}};
-
-  auto item = map.find(name);
-  if (item != map.end())
-    return item->second;
-
-  return BlendOp::kUnknown;
+  if (name == "add")
+    return BlendOp::kAdd;
+  else if (name == "substract")
+    return BlendOp::kSubtract;
+  else if (name == "reverse_substract")
+    return BlendOp::kReverseSubtract;
+  else if (name == "min")
+    return BlendOp::kMin;
+  else if (name == "max")
+    return BlendOp::kMax;
+  else if (name == "zero")
+    return BlendOp::kZero;
+  else if (name == "src")
+    return BlendOp::kSrc;
+  else if (name == "dst")
+    return BlendOp::kDst;
+  else if (name == "src_over")
+    return BlendOp::kSrcOver;
+  else if (name == "dst_over")
+    return BlendOp::kDstOver;
+  else if (name == "src_in")
+    return BlendOp::kSrcIn;
+  else if (name == "dst_in")
+    return BlendOp::kDstIn;
+  else if (name == "src_out")
+    return BlendOp::kSrcOut;
+  else if (name == "dst_out")
+    return BlendOp::kDstOut;
+  else if (name == "src_atop")
+    return BlendOp::kSrcAtop;
+  else if (name == "dst_atop")
+    return BlendOp::kDstAtop;
+  else if (name == "xor")
+    return BlendOp::kXor;
+  else if (name == "multiply")
+    return BlendOp::kMultiply;
+  else if (name == "screen")
+    return BlendOp::kScreen;
+  else if (name == "overlay")
+    return BlendOp::kOverlay;
+  else if (name == "darken")
+    return BlendOp::kDarken;
+  else if (name == "lighten")
+    return BlendOp::kLighten;
+  else if (name == "color_dodge")
+    return BlendOp::kColorDodge;
+  else if (name == "color_burn")
+    return BlendOp::kColorBurn;
+  else if (name == "hard_light")
+    return BlendOp::kHardLight;
+  else if (name == "soft_light")
+    return BlendOp::kSoftLight;
+  else if (name == "difference")
+    return BlendOp::kDifference;
+  else if (name == "exclusion")
+    return BlendOp::kExclusion;
+  else if (name == "invert")
+    return BlendOp::kInvert;
+  else if (name == "invert_rgb")
+    return BlendOp::kInvertRGB;
+  else if (name == "linear_dodge")
+    return BlendOp::kLinearDodge;
+  else if (name == "linear_burn")
+    return BlendOp::kLinearBurn;
+  else if (name == "vivid_light")
+    return BlendOp::kVividLight;
+  else if (name == "linear_light")
+    return BlendOp::kLinearLight;
+  else if (name == "pin_light")
+    return BlendOp::kPinLight;
+  else if (name == "hard_mix")
+    return BlendOp::kHardMix;
+  else if (name == "hsl_hue")
+    return BlendOp::kHslHue;
+  else if (name == "hsl_saturation")
+    return BlendOp::kHslSaturation;
+  else if (name == "hsl_color")
+    return BlendOp::kHslColor;
+  else if (name == "hsl_luminosity")
+    return BlendOp::kHslLuminosity;
+  else if (name == "plus")
+    return BlendOp::kPlus;
+  else if (name == "plus_clamped")
+    return BlendOp::kPlusClamped;
+  else if (name == "plus_clamped_alpha")
+    return BlendOp::kPlusClampedAlpha;
+  else if (name == "plus_darker")
+    return BlendOp::kPlusDarker;
+  else if (name == "minus")
+    return BlendOp::kMinus;
+  else if (name == "minus_clamped")
+    return BlendOp::kMinusClamped;
+  else if (name == "contrast")
+    return BlendOp::kContrast;
+  else if (name == "invert_ovg")
+    return BlendOp::kInvertOvg;
+  else if (name == "red")
+    return BlendOp::kRed;
+  else if (name == "green")
+    return BlendOp::kGreen;
+  else if (name == "blue")
+    return BlendOp::kBlue;
+  else
+    return BlendOp::kUnknown;
 }
 
 }  // namespace amber
