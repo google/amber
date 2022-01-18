80f2a15f40834cef4e1f42617453e6a506dd2f9e
Ari Suonpää <ari.suonpaa@siru.fi>
2022-01-18 13:31:00 +0200

    Updated windows build script

--- kokoro/scripts/windows/build.bat
+++ kokoro/scripts/windows/build.bat
@@ -45,7 +45,7 @@ cd build
 echo "Build and install SPIR-V headers. %DATE% %TIME%"
 mkdir -p spirv-headers
 cd spirv-headers
-cmake -GNinja -DCMAKE_INSTALL_PREFIX=.. -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DCMAKE_C_COMPILER=cl.exe -DCMAKE_CXX_COMPILER=cl.exe ..\..\third_party\spirv-headers\
+cmake -GNinja -DCMAKE_INSTALL_PREFIX=.. -DCMAKE_BUILD_TYPE=%BUILD_TYPE% -DCMAKE_C_COMPILER=cl.exe -DCMAKE_CXX_COMPILER=cl.exe ..\..\third_party\spirv-headers\
 if %ERRORLEVEL% GEQ 1 exit /b %ERRORLEVEL%
 cmake --build . --target install
 if %ERRORLEVEL% GEQ 1 exit /b %ERRORLEVEL%
@@ -53,7 +53,7 @@ cd ..
 echo "Build and install SPIR-V tools. %DATE% %TIME%"
 mkdir -p spirv-tools
 cd spirv-tools
-cmake -GNinja -DCMAKE_INSTALL_PREFIX=.. -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DCMAKE_C_COMPILER=cl.exe -DCMAKE_CXX_COMPILER=cl.exe -DSPIRV-Headers_SOURCE_DIR=%SRC%\third_party\spirv-headers -DSPIRV_SKIP_TESTS=ON ..\..\third_party\spirv-tools
+cmake -GNinja -DCMAKE_INSTALL_PREFIX=.. -DCMAKE_BUILD_TYPE=%BUILD_TYPE% -DCMAKE_C_COMPILER=cl.exe -DCMAKE_CXX_COMPILER=cl.exe -DSPIRV-Headers_SOURCE_DIR=%SRC%\third_party\spirv-headers -DSPIRV_SKIP_TESTS=ON ..\..\third_party\spirv-tools
 if %ERRORLEVEL% GEQ 1 exit /b %ERRORLEVEL%
 cmake --build . --target install
 if %ERRORLEVEL% GEQ 1 exit /b %ERRORLEVEL%
