d266f9c58b498632b302cb3230900e6016ef1f07
David Neto <dneto@google.com>
2025-01-23 17:51:58 -0500

    kokoro: use gcc 13

--- kokoro/linux-gcc-release-clspv/build.sh
+++ kokoro/linux-gcc-release-clspv/build.sh
@@ -17,6 +17,6 @@ set -e  # fail on error
 set -x  # display commands
 
 SCRIPT_DIR=`dirname "$BASH_SOURCE"`
-source $SCRIPT_DIR/../scripts/linux/build.sh RELEASE "gcc-9" \
+source $SCRIPT_DIR/../scripts/linux/build.sh RELEASE "gcc-13" \
     -DAMBER_USE_CLSPV=TRUE \
     -DAMBER_ENABLE_SWIFTSHADER=TRUE
